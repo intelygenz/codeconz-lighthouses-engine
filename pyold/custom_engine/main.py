e210e2f0cc86aa6e94406ff192071e8fdddfbe27
Gabriel Munoz <gabriel.munoz@intelygenz.com>
2024-07-16 12:50:41 +0200

    add base implementation for run function for training

--- pyold/custom_engine/main.py
+++ pyold/custom_engine/main.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import engine, interface
+import engine, train
 
 from bots.randbot import RandBot
 from bots.reinforce import REINFORCE
 
-import ipdb
 
 # ==============================================================================
 # MAIN
@@ -18,10 +17,10 @@ if __name__ == "__main__":
     cfg_file = "maps/grid.txt"
     
     # Set the bots to play the game
-    bots = [REINFORCE(), RandBot()]
+    bots = [RandBot(), RandBot()]
 
     config = engine.GameConfig(cfg_file)
     game = engine.Game(config, len(bots))
 
-    iface = interface.Interface(game, bots)
-    iface.train_reinforce(n_training_episodes=10, max_t=100,)
+    iface = train.Interface(game, bots, debug=False)
+    iface.run()
--- pyold/custom_engine/train.py
+++ pyold/custom_engine/train.py
@@ -1,97 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import engine, view
-from collections import deque
+
 import numpy as np
 
-# ==============================================================================
-# ROBOT
-# Los robots definidos deben heredar de esta clase.
-# ==============================================================================
+import engine, view
 
-class Bot(object):
-    """Bot base. Este bot no hace nada (pasa todos los turnos)."""
-    NAME = "NullBot"
-
-    # ==========================================================================
-    # Comportamiento del bot
-    # Métodos a implementar / sobreescribir (opcionalmente)
-    # ==========================================================================
-
-    def __init__(self):
-        """Inicializar el bot: llamado al comienzo del juego."""
-        pass
-
-    def play(self, state):
-        """Jugar: llamado cada turno.
-        Debe devolver una acción (jugada).
-        
-        state: estado actual del juego.
-        """
-        return self.nop()
-
-    def success(self):
-        """Éxito: llamado cuando la jugada previa es válida."""
-        pass
-
-    def error(self, message, last_move):
-        """Error: llamado cuando la jugada previa no es válida."""
-        print("Recibido error: %s", message)
-        print("Jugada previa: %r", last_move)
-
-    # ==========================================================================
-    # Jugadas posibles
-    # No es necesario sobreescribir estos métodos.
-    # ==========================================================================
-
-    def nop(self):
-        """Pasar el turno"""
-        return {
-            "command": "pass",
-        }
-
-    def move(self, x, y):
-        """Mover a una casilla adyacente
-        x: delta x (0, -1, 1)
-        y: delta y (0, -1, 1)
-        """
-        return {
-            "command": "move",
-            "x": x,
-            "y": y
-        }
-
-    def attack(self, energy):
-        """Atacar a un faro
-        energy: energía (entero positivo)
-        """
-        return {
-            "command": "attack",
-            "energy": energy
-        }
-
-    def connect(self, destination):
-        """Conectar a un faro remoto
-        destination: tupla o lista (x,y): coordenadas del faro remoto
-        """
-        return {
-            "command": "connect",
-            "destination": destination
-        }
-
-
-# ==============================================================================
-# Interfaz
-# ==============================================================================
 
 class CommError(Exception):
     pass
 
 
 class Interface(object):
-    def __init__(self, game, bots):
+    def __init__(self, game, bots, debug=False):
         self.game = game
         self.bots = bots
+        self.debug = debug
 
     def turn(self, player, move):
         if not isinstance(move, dict) or "command" not in move:
@@ -124,123 +47,104 @@ class Interface(object):
         except engine.MoveError as e:
             return {"success": False, "message": str(e)}
 
+    def get_state(self, player):
+        # Lighthouses info extraction
+        lighthouses = []
+        for lh in self.game.lighthouses.values():
+            connections = [next(l for l in c if l is not lh.pos)
+                            for c in self.game.conns if lh.pos in c]
+            lighthouses.append({
+                "position": lh.pos,
+                "owner": lh.owner,
+                "energy": lh.energy,
+                "connections": connections,
+                "have_key": lh.pos in player.keys,
+            })
+
+        # Extract the fields for calculating the state
+        player_view = self.game.island.get_view(player.pos)
+
+        state =  {
+            "position": player.pos,
+            "score": player.score,
+            "energy": player.energy,
+            "view": player_view,
+            "lighthouses": lighthouses
+        }
+
+        return state
+
+    def estimate_reward(self, bot):
+        """
+        The logic for estimating the reward is the difference of score between
+        two consecutive actions
+        """
+        len_scores = len(bot.scores)
+        score_diff = bot.scores[-1] - bot.scores[len_scores - 2]
+
+        return np.clip(0, 1, score_diff)
+
     def run(self):
         game_view = view.GameView(self.game)
-
-        ########################################
-        # Send initial state to every bot
-        ########################################
-        
-        ###########################################
-        # TODO: The initial state must be the same for all the bots, no matter the type
-        # If the state is different, it has to be treated in a custom function (eg: initialize game)
-        ###########################################
-
-        player_idx = 0
-        for bot in self.bots:
-            # Every bot receives initial state
-            self.bots[player_idx].player_num = self.game.players[player_idx].num
-            self.bots[player_idx].player_count = len(self.game.players)
-            self.bots[player_idx].position = self.game.players[player_idx].pos
-            self.bots[player_idx].map = self.game.island.map
-            self.bots[player_idx].lighthouses = map(tuple, 
-                                                    list(self.game.lighthouses.keys()))
-
-            if self.bots[player_idx].NAME[0] == 'REINFORCE':
-                player = self.game.players[player_idx]
-                lighthouses = []
-                for lh in self.game.lighthouses.values():
-                    connections = [next(l for l in c if l is not lh.pos)
-                                    for c in self.game.conns if lh.pos in c]
-                    lighthouses.append({
-                        "position": lh.pos,
-                        "owner": lh.owner,
-                        "energy": lh.energy,
-                        "connections": connections,
-                        "have_key": lh.pos in player.keys,
-                    })
-
-                state =  {
-                    "position": player.pos,
-                    "score": player.score,
-                    "energy": player.energy,
-                    "view": self.game.island.get_view(player.pos),
-                    "lighthouses": lighthouses
-                }
-
-                ###########################################
-                # TODO: The function initialize_game has to return the state pre-processed because we need to store it
-                ###########################################
-
-                bot.initialize_game(state)
-
-            player_idx += 1
-
-        ###########################################
-        # TODO: This loop has to be within a finite number of episodes and must be "not done" instead of "True"
-        ###########################################
         round = 0
         while True:
             self.game.pre_round()
             game_view.update()
-            
+
             player_idx = 0
             for bot in self.bots:
                 player = self.game.players[player_idx]
 
-                ###########################################
-                # TODO: Function to pre-process state
-                ###########################################
-                lighthouses = []
-                for lh in self.game.lighthouses.values():
-                    connections = [next(l for l in c if l is not lh.pos)
-                                    for c in self.game.conns if lh.pos in c]
-                    lighthouses.append({
-                        "position": lh.pos,
-                        "owner": lh.owner,
-                        "energy": lh.energy,
-                        "connections": connections,
-                        "have_key": lh.pos in player.keys,
-                    })
-
-                state =  {
-                    "position": player.pos,
-                    "score": player.score,
-                    "energy": player.energy,
-                    "view": self.game.island.get_view(player.pos),
-                    "lighthouses": lighthouses
-                }
-
-                ###########################################
-                # TODO: Get action logits from actor and value from critic
-                ###########################################
-                move = bot.play(state)
-
-                ###########################################
-                # TODO: Get action probability
-                ###########################################
-
-                ###########################################
-                # TODO: Store prediction and action/probabilities
-                ###########################################
-
-                ###########################################
-                # TODO: Execute action and get rewards and next state
-                ###########################################
-                status = self.turn(player, move)
-
-                if status["success"]:
-                    bot.success()
+                ####################################################
+                # If round 0, Get initial state and initialize bot
+                ####################################################
+                if round == 0:
+                    bot.player_num = player.num
+                    bot.map = self.game.island.map
+                    state = self.get_state(player)
+                    bot.initialize_game(state)
                 else:
-                    bot.error(status["message"], move)
+                    state = next_state
 
+                ###########################################
+                # Get action
+                ###########################################
+                action = bot.play(state)
+
+                ###########################################
+                # Execute action and get rewards and next state
+                ###########################################
+                status = self.turn(player, action)
+
+                if self.debug:
+                    try:
+                        bot.error(status["message"], action)
+                    except:
+                        pass
+
+                bot.scores.append(player.score)
+                reward = self.estimate_reward(bot)
                 game_view.update()
+
+                next_state = self.get_state(player)
+                transition = [state, action, reward, next_state]
+                bot.transitions.append(transition)
+
                 player_idx += 1
 
             self.game.post_round()
-            
+
             ###########################################
-            # TODO: Print the scores after each round
+            # TODO: Optimize models
             ###########################################
+
+            ###########################################
+            # Print the scores after each round
+            ###########################################
+
+            s = "########### ROUND %d SCORE: " % round
+            for i in range(len(self.bots)):
+                s += "P%d: %d " % (i, self.game.players[i].score)
+            print(s)
 
             round += 1
