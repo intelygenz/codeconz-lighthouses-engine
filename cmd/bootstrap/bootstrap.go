1f085acf2105b3060872b8bfa2d461f479e9ffcb
dovixman <me@dovixman.io>
2024-11-11 18:16:09 +0100

    add configuration file to bot

--- cmd/bootstrap/bootstrap.go
+++ cmd/bootstrap/bootstrap.go
@@ -3,6 +3,7 @@ package bootstrap
 import (
 	"fmt"
 	"net"
+	"os"
 	"time"
 
 	"github.com/jonasdacruz/lighthouses_aicontest/internal/engine/game"
@@ -68,11 +69,12 @@ func (b *Bootstrap) Run() {
 func (b *Bootstrap) initializeConfiguration() {
 	viper.AutomaticEnv()
 
-	viper.SetDefault("game.listen_address", ":50051")
-	viper.SetDefault("game.join_timeout", 5*time.Second)
-	viper.SetDefault("game.turn_request_timeout", 100*time.Millisecond)
-	viper.SetDefault("game.turns", 15)
-	viper.SetDefault("game.board_path", "./maps/island_simple.txt")
-	viper.SetDefault("game.verbosity", true)
-	viper.SetDefault("game.time_between_rounds", 1*time.Second) // 0 to avoid any sleeping
+	viper.AddConfigPath("./")
+	viper.SetConfigName("cfg")
+	viper.SetConfigType("yaml")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		os.Exit(1)
+	}
 }
