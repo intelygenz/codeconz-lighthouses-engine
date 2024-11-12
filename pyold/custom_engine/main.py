9acee2b346b5e9e92b9d0fdb4be1ed795cb8a5dd
colleenmcmillon <colleen.mcmillon@intelygenz.com>
2024-11-12 08:47:22 +0100

    delete reinforce scripts and rename ppo scripts

--- pyold/custom_engine/main.py
+++ pyold/custom_engine/main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import engine, train
+import engine, train as train
 import pandas as pd
 import os
 
+from bots.ppo import PPO
 from bots.randbot import RandBot
-from bots.reinforce import REINFORCE
 
 
 # ==============================================================================
@@ -16,46 +16,61 @@ from bots.reinforce import REINFORCE
 
 if __name__ == "__main__":
     # Set the map
-    cfg_file = "maps/grid.txt"
-    
+    cfg_files_train = ["./maps/game_map.txt"] #, "maps/island.txt", "maps/random.txt"] #, "maps/square_xl.txt"]
+    cfg_file_eval = "./maps/game_map.txt"
     # Set the bots to play the game
 
     # REINFORCE(state_maps=False, trained_model_filename='reinforce_mlp.pth', save_model_filename = "reinforce_mlp.pth", use_saved_model=True),
     #         REINFORCE(state_maps=False, trained_model_filename=None, save_model_filename = None, use_saved_model=False),
     #         REINFORCE(state_maps=False, trained_model_filename=None, save_model_filename = None, use_saved_model=False),
 
-    bots = [REINFORCE(state_maps=True, model_filename = "reinforce_test_cnn_transfer.pth", use_saved_model=False),]
-    #bots = [RandBot()]
-
-    NUM_EPISODES = 2
-    MAX_AGENT_UPDATES = 5 # Number of times to update the agent 
+    NUM_EPISODES = 10
+    MAX_AGENT_UPDATES = 90 # Number of times to update the agent within an episode
     NUM_STEPS_POLICY_UPDATE = 128 # Number of experiences to collect for each update to the agent
-    MAX_EVALUATION_ROUNDS = 10
+    MAX_EVALUATION_ROUNDS = 1000
     TRAIN = True # Whether to run training or evaluation
+    NUM_ENVS = 1
+    STATE_MAPS = True
+    MODEL_FILENAME = "ppo_transfer_cnn_1envs_new_rew.pth"
+    USE_SAVED_MODEL = False
+    MAX_TOTAL_UPDATES = NUM_EPISODES * MAX_AGENT_UPDATES
 
     #######################################################################
     # Total number of rounds = MAX_AGENT_UPATES * NUM_STEPS_POLICY_UPDATE #
     #######################################################################
 
-    for i in range(1, NUM_EPISODES+1):
-        config = engine.GameConfig(cfg_file)
-        game = engine.Game(config, len(bots))
+    #bots = [REINFORCE(state_maps=False, model_filename = "reinforce.pth", use_saved_model=True),]
+    bots = [PPO(state_maps=STATE_MAPS, num_envs=NUM_ENVS, num_steps=NUM_STEPS_POLICY_UPDATE, num_updates=MAX_TOTAL_UPDATES, train=TRAIN, model_filename = MODEL_FILENAME, use_saved_model=USE_SAVED_MODEL),]
 
-        iface = train.Interface(game, bots, debug=False)
-        if TRAIN:
-            iface.train(max_updates=MAX_AGENT_UPDATES, num_steps_update=NUM_STEPS_POLICY_UPDATE)
-        else:
+    if TRAIN:
+        for i in range(1, NUM_EPISODES+1):
+            for i in range(len(cfg_files_train)):
+                config = engine.GameConfig(cfg_files_train[i])
+                game = [engine.Game(config, len(bots)) for i in range(NUM_ENVS)]
+            # config = engine.GameConfig(cfg_file)
+            # game = [engine.Game(config, len(bots)) for i in range(NUM_ENVS)]
+
+                iface = train.Interface(game, bots, debug=False)
+                iface.train(max_updates=MAX_AGENT_UPDATES, num_steps_update=NUM_STEPS_POLICY_UPDATE)
+            # iface = train_ppo.Interface(game, bots, debug=False)
+            # iface.train(max_updates=MAX_AGENT_UPDATES, num_steps_update=NUM_STEPS_POLICY_UPDATE)
+    
+    if not TRAIN:
+        for i in range(1, NUM_EPISODES+1):
+            config = engine.GameConfig(cfg_file_eval)
+            game = [engine.Game(config, len(bots))]
+
+            iface = train.Interface(game, bots, debug=False)
             iface.run(max_rounds=MAX_EVALUATION_ROUNDS)
             final_scores_list = []
             for bot in bots:
-                bot.final_scores_list.append(bot.scores[-1])
-    
-    if not TRAIN:
+                bot.final_scores_list.append(bot.scores[-1][0])
+        
         final_scores = pd.DataFrame()
         for bot in bots:
                 final_scores["bot_"+str(bot.player_num)] = bot.final_scores_list
         os.makedirs('./final_scores', exist_ok=True)
-        final_scores.to_csv('./final_scores/final_scores_cnn_no_trained.csv', index_label='episode')
+        final_scores.to_csv('./final_scores/final_scores_cnn_1env_extra_connect3_update.csv', index_label='episode')
 
 
 
--- pyold/custom_engine/train.py
+++ pyold/custom_engine/train.py
@@ -32,9 +32,9 @@ class Interface(object):
             elif move["command"] == "attack":
                 if "energy" not in move or not isinstance(move["energy"], int):
                     raise engine.MoveError("Attack command requires integer energy")
-                if player.pos not in self.game.lighthouses:
+                if player.pos not in self.game[0].lighthouses:
                     raise engine.MoveError("Player must be located at target lighthouse")
-                self.game.lighthouses[player.pos].attack(player, move["energy"])
+                self.game[0].lighthouses[player.pos].attack(player, move["energy"])
             elif move["command"] == "connect":
                 if "destination" not in move:
                     raise engine.MoveError("Connect command requires destination")
@@ -43,19 +43,19 @@ class Interface(object):
                     hash(dest)
                 except:
                     raise engine.MoveError("Destination must be a coordinate pair")
-                self.game.connect(player, dest)
+                self.game[0].connect(player, dest)
             else:
                 raise engine.MoveError("Invalid command %r" % move["command"])
             return {"success": True}
         except engine.MoveError as e:
             return {"success": False, "message": str(e)}
 
-    def get_state(self, player):
+    def get_state(self, player, i):
         # Lighthouses info extraction
         lighthouses = []
-        for lh in self.game.lighthouses.values():
+        for lh in self.game[i].lighthouses.values():
             connections = [next(l for l in c if l is not lh.pos)
-                            for c in self.game.conns if lh.pos in c]
+                            for c in self.game[i].conns if lh.pos in c]
             lighthouses.append({
                 "position": lh.pos,
                 "owner": lh.owner,
@@ -65,7 +65,7 @@ class Interface(object):
             })
 
         # Extract the fields for calculating the state
-        player_view = self.game.island.get_view(player.pos)
+        player_view = self.game[i].island.get_view(player.pos)
 
         state =  {
             "position": player.pos,
@@ -77,93 +77,67 @@ class Interface(object):
 
         return state
 
-    def estimate_reward_old(self, bot):
-        """
-        The logic for estimating the reward is the difference of score between
-        two consecutive actions
-        """
-        len_scores = len(bot.scores)
-        score_diff = bot.scores[-1] - bot.scores[len_scores - 2]
-
-        return np.clip(0, 1, score_diff)
     
-    def estimate_reward(self, action, state, next_state, player, status):
+    def estimate_reward(self, action, state, next_state, player, status, scores, i):
         """
         The logic for estimating the reward is the following:
         1. if "status" is False: -1
-        2. if "move" and the move is invalid: -1
-        3. if "move" and land on lighthouse that do not own: 0.5
-        4. if "move" and increase bot's energy: 0.3
-        5. if "attack" and not on a lighthouse: -1
-        6. if "attack" and gain control of lighthouse: 0.65
-        7. if "attack" and already control lighthouse: 0.15
-        8. if "attack" and don't gain control of lighthouse: 0
-        9. if "connect" and not on a lighthouse: -1
-        10. if "connect" and no new connection (connection not possible): 0
-        11. if "connect" and connect three lighthouses: 1
-        12. if "connect" and connect two lighthouses: 0.85 
+        2. if command is "move": -1
+        3. if "attack" and gain control of lighthouse: 0 (can also try 0.2 and 0.45)
+        4. if "attack" and do not gain control: -1
+        5. if "connect" and connect three lighthouses: 1 (can also try adding extra amount from score gained)
+        6. if "connect" and connect two lighthouses: 0 (can also try 0.5 and 0.85)
+        7. if "connect" and do not connect lighthouses: -1
+        8. if command is "pass": -1
+        9. anything not covered by the above: -1
         """
+        # TODO: this reward function should be simplified once the final one has been decided on
         state_lh = dict((tuple(lh["position"]), lh) for lh in state["lighthouses"])
         next_state_lh = dict((tuple(lh["position"]), lh) for lh in next_state["lighthouses"])
+        # Get difference between previous score and current score
+        score_temp = []
+        len_scores = len(scores)
+        for j in range(len(scores)):
+            score_temp.append(scores[j][i])
+        score_diff = score_temp[-1] - score_temp[len_scores - 2]
+        # Create small extra component to take into account the size of triangle created
+        extra = score_diff*0.01 
 
         # If status is False
         if status['success'] == False:
             return -1
-        ### MOVE ###
-        # If the move command is invalid
+        # If the command is move
         elif action['command'] == "move":
-            # If the move command is invalid
-            if (state['position'][0] == next_state['position'][0]) and (state['position'][1] == next_state['position'][1]):
-                return -1
-            #If move and land on a lighthouse not owned by player
-            elif next_state['position'] in state_lh.keys() and state_lh[next_state['position']]['owner'] != player.num:
-                return 0.55
-            # If move and increase bot's energy
-            elif next_state['energy'] > state['energy']:
-                return 0.35 
-            else:
-                return 0
+            return -1
         ### ATTACK ###
         elif action['command'] == "attack":
-            # If attack and not on a lighthouse
-            if state['position'] not in state_lh.keys():
-                return -1 
             # If attack a lighthouse and gain control of it
-            elif state_lh[state['position']]['owner'] != player.num and next_state_lh[next_state['position']]['owner'] == player.num:
-                return 0.7 
-            # If attack a lighthouse and already control it
-            elif state_lh[state['position']]['owner'] == player.num :
-                return 0.15 
-            # If attack a lighthouse and not enough energy to gain control
-            elif state_lh[state['position']]['owner'] != player.num and next_state_lh[next_state['position']]['owner'] != player.num:
-                return 0
+            if state_lh[state['position']]['owner'] != player.num and next_state_lh[next_state['position']]['owner'] == player.num:
+                return 0 # 0.2 
+            else: 
+                return -1
         ### CONNECT ###
-        elif action['command'] == "connect":
-            # If try to connect and not on a lighthouse
-            if state['position'] not in state_lh.keys():
-                return -1 
-            # If try to connect and connection not possible (ex. doesn't own lighthouse, doesn't have key to any other lighthouse, etc.)
-            elif (state_lh[state['position']]['owner'] != player.num or 
-                  state_lh[state['position']]["connections"] == next_state_lh[next_state['position']]["connections"]): 
-                return -0.85
+        elif action['command'] == "connect":   
             # If connect lighthouses
-            elif (state_lh[state['position']]['owner'] == player.num and 
+            if (state_lh[state['position']]['owner'] == player.num and 
                   len(state_lh[state['position']]["connections"]) < len(next_state_lh[next_state['position']]["connections"])):
                 # If connect three lighthouses
                 new_connection = list(set(next_state_lh[next_state['position']]["connections"])-set(state_lh[state['position']]["connections"]))[0]
                 if any(i in next_state_lh[next_state['position']]["connections"] for i in next_state_lh[new_connection]["connections"]):
-                    return 1
+                    return 1 # + extra
                 # If connect two lighthouses
                 else:
-                    return 0.85       
+                    return 0 #0.5
+            else:
+                return -1      
         elif action['command'] == "pass":
             return -1
         else:
-            return 0
+            return -1
     
 
-    def train(self, max_updates=1000000000000, num_steps_update=256):
-        game_view = view.GameView(self.game)
+    def train(self, max_updates=0, num_steps_update=0):
+        game_view = [view.GameView(self.game[i]) for i in range(len(self.game))]
         update = 0
         round = 0
         running = True
@@ -172,71 +146,80 @@ class Interface(object):
             ###################################
             # Get experiences to update agent #
             ###################################
-            for i in range(num_steps_update):
+
+            for step in range(num_steps_update):
                 # Event handler for game engine
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         running = False
 
-                self.game.pre_round()
-                game_view.update()
+                for i in range(len(self.game)):
+                    self.game[i].pre_round()
+                    game_view[i].update()
 
                 player_idx = 0
                 for bot in self.bots:
-                    player = self.game.players[player_idx]
+                    player = [self.game[i].players[player_idx] for i in range(len(self.game))]
 
                     ####################################################
                     # If round 0, Get initial state and initialize bot
                     ####################################################
                     if round == 0:
-                        bot.player_num = player.num
-                        bot.map = self.game.island.map
-                        state = self.get_state(player)
+                        bot.player_num = player[0].num
+                        bot.map = [self.game[i].island.map for i in range(len(self.game))]
+                        state = [self.get_state(player[i], i) for i in range(len(self.game))]
                         bot.initialize_game(state)
                     else:
                         state = next_state
+                    if step == 0:
+                        bot.initialize_experience_gathering()
 
                     ###########################################
                     # Get action
                     ###########################################
-                    action = bot.play(state)
+                    action = bot.play(state, step)
                     ###########################################
                     # Execute action and get rewards and next state
                     ###########################################
-                    status = self.turn(player, action)
+                    status = [self.turn(player[i], action[i]) for i in range(len(self.game))]
 
                     if self.debug:
                         try:
                             bot.error(status["message"], action)
                         except:
                             pass
-
-                    bot.scores.append(player.score)
-                    game_view.update()
-
-                    next_state = self.get_state(player)
-                    reward = self.estimate_reward(action, state, next_state, player, status)
+                    
+                    scores_temp = []
+                    for i in range(len(self.game)):         
+                        scores_temp.append(player[i].score)
+                        game_view[i].update()
+                    
+                    bot.scores.append(scores_temp)
+                    next_state = [self.get_state(player[i], i) for i in range(len(self.game))]
+                    reward = [self.estimate_reward(action[i], state[i], next_state[i], player[i], status[i], bot.scores, i) for i in range(len(self.game))]
                     transition = [state, action, reward, next_state]
                     bot.transitions.append(transition)
                     bot.transitions_temp.append(transition)
 
                     player_idx += 1
-
-                self.game.post_round()
+                
+                for i in range(len(self.game)):
+                    self.game[i].post_round()
 
                 ###########################################
                 # Print the scores after each round
                 ###########################################
 
-                s = "########### ROUND %d SCORE: " % round
-                for i in range(len(self.bots)):
-                    s += "P%d: %d " % (i, self.game.players[i].score)
-                print(s)
+                # s = "########### ROUND %d SCORE: " % round
+                # for i in range(len(self.bots)):
+                #     s += "P%d: %d " % (i, self.game[i].players[i].score)
+                # print(s)
 
                 round += 1
             
             update += 1
-            print("update: ", update)
+            bot.update += 1
+            print("update", bot.update)
                 
             ###########################################
             # Optimize models
@@ -244,7 +227,7 @@ class Interface(object):
             for bot in self.bots:
                 bot.optimize_model(bot.transitions_temp)
                 bot.transitions_temp = []
-                print("updated policy")
+                #print("updated policy")
             
                 policy_loss = pd.DataFrame()
                 policy_loss[str(bot.player_num)] = bot.policy_loss_list
@@ -254,30 +237,32 @@ class Interface(object):
                 bot.save_trained_model()
     
     def run(self, max_rounds=None):
-        game_view = view.GameView(self.game)
+        game_view = [view.GameView(self.game[i]) for i in range(len(self.game))]
         round = 0
         running = True
         
         while round < max_rounds and running: 
             # Event handler for game engine
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
+                    if event.type == pygame.QUIT:
+                        running = False
 
-            self.game.pre_round()
-            game_view.update()
+            for i in range(len(self.game)):
+                self.game[i].pre_round()
+                game_view[i].update()
 
             player_idx = 0
             for bot in self.bots:
-                player = self.game.players[player_idx]
+                player = [self.game[i].players[player_idx] for i in range(len(self.game))]
+            
 
                 ####################################################
                 # If round 0, Get initial state and initialize bot
                 ####################################################
                 if round == 0:
-                    bot.player_num = player.num
-                    bot.map = self.game.island.map
-                    state = self.get_state(player)
+                    bot.player_num = player[0].num
+                    bot.map = [self.game[i].island.map for i in range(len(self.game))]
+                    state = [self.get_state(player[i], i) for i in range(len(self.game))]
                     bot.initialize_game(state)
                 else:
                     state = next_state
@@ -289,26 +274,29 @@ class Interface(object):
                 ###########################################
                 # Execute action and get rewards and next state
                 ###########################################
-                status = self.turn(player, action)
+                status = [self.turn(player[i], action[i]) for i in range(len(self.game))]
 
                 if self.debug:
                     try:
                         bot.error(status["message"], action)
                     except:
                         pass
-
-                bot.scores.append(player.score)
-                game_view.update()
-
-                next_state = self.get_state(player)
-                reward = self.estimate_reward(action, state, next_state, player, status)
+                for i in range(len(self.game)):
+                        scores_temp = []
+                        scores_temp.append(player[i].score)
+                        game_view[i].update()
+                
+                bot.scores.append(scores_temp)
+                
+                next_state = [self.get_state(player[i], i) for i in range(len(self.game))]
+                reward = [self.estimate_reward(action[i], state[i], next_state[i], player[i], status[i], bot.scores, i) for i in range(len(self.game))]
                 transition = [state, action, reward, next_state]
                 bot.transitions.append(transition)
-                bot.transitions_temp.append(transition)
-
+             
                 player_idx += 1
 
-            self.game.post_round()
+            for i in range(len(self.game)):
+                self.game[i].post_round()
 
             ###########################################
             # Print the scores after each round
@@ -316,7 +304,7 @@ class Interface(object):
 
             s = "########### ROUND %d SCORE: " % round
             for i in range(len(self.bots)):
-                s += "P%d: %d " % (i, self.game.players[i].score)
+                s += "P%d: %d " % (i, self.game[i].players[i].score)
             print(s)
 
             round += 1
